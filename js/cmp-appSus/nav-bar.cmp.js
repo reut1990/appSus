0a948606aa393e98cd269f81aa534108c25843c2
reut <reut1990@gmail.com>
2018-11-09 22:52:32 +0200

    finish home page and nav design , and adjusting to mobile

--- js/cmp-appSus/nav-bar.cmp.js
+++ js/cmp-appSus/nav-bar.cmp.js
@@ -4,14 +4,14 @@ export default {
     <section class="top-nav">
        <div class="inside-container">
             <div class="top-nav-logo logo" title="" alt="Blogin">Miss APPSUS</div>
-            <!-- <div class="screen" v-on:click="toggleMenu"></div> -->
+            <div class="screen" v-on:click="toggleMenu"></div>
             <nav class="main-nav-container" id="main-nav">
-                    <router-link exact to="/home"><div  class="main-nav-item first">Home</div></router-link>
-                    <router-link exact to="/about"><div  class="main-nav-item" >About</div></router-link> 
-                    <router-link exact to="/missKeep"><div  class="main-nav-item" >Miss Keep</div></router-link> 
-                    <router-link exact to="/mrEmails"><div class="main-nav-item" >Mr Emails</div></router-link> 
+                    <router-link exact to="/home"><div  v-on:click="toggleMenu" class="main-nav-item first">Home</div></router-link>
+                    <router-link exact to="/about"><div  v-on:click="toggleMenu" class="main-nav-item" >About</div></router-link> 
+                    <router-link exact to="/missKeep"><div v-on:click="toggleMenu" class="main-nav-item" >Miss Keep</div></router-link> 
+                    <router-link exact to="/mrEmails"><div v-on:click="toggleMenu" class="main-nav-item" >Mr Emails</div></router-link> 
             </nav>
-           <!-- <button class="menu-icon" v-on:click="toggleMenu">☰</button> -->
+           <button class="menu-icon" v-on:click="toggleMenu">☰</button>
         </div>
     </section>
     `,
@@ -22,7 +22,12 @@ export default {
     },
 
     methods: {
-
+        toggleMenu(){
+            document.body.classList.toggle('open');
+            if(document.body.classList.contains('open')) document.querySelector('.menu-icon').innerText="✖";
+            else  document.querySelector('.menu-icon').innerText='☰';
+        },
+       
     },
     computed: {
 
